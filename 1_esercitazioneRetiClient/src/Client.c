ff91887e429bbffdb0e28058f1e01294c78e84c8
Mirco Sipone <mircosipone@gmail.com>
2019-10-12 13:49:47 +0200

    cv

---
+++ 1_esercitazioneRetiClient/src/Client.c
@@ -0,0 +1,150 @@
+/*
+ ============================================================================
+ Name        : 1_esercitazioneRetiClient.c
+ Author      : 
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#if defined WIN32
+#include <winsock.h>
+#else
+#define closesocket close
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#endif
+#include <stdio.h>
+#include <stdlib.h>
+#define BUFFERSIZE 512
+#define PROTOPORT 27015
+
+void errorHandler(char *errorMessage)
+{
+	printf("%s", errorMessage);
+}
+
+void ClearWinSock()
+{
+	#if defined WIN32
+	WSACleanup();
+	#endif
+}
+
+int sendToClient(int clientSocket, const char *message)
+{
+	int lungMessage = strlen(message);
+	if(send(clientSocket, message, lungMessage, 0) != lungMessage)
+	{
+		errorHandler("send() sent a different number of bytes than expected.\n");
+		closesocket(clientSocket);
+		ClearWinSock();
+		return -1;
+	}
+	return 0;
+}
+
+void sendSuccess(int clientSocket, const char *message)
+{
+	int success;
+	success = sendToClient(clientSocket, message);
+}
+
+int main(void)
+{
+	#if defined WIN32
+	WSADATA wsaData;
+	int iResult = WSAStartup(MAKEWORD(2,2), &wsaData);
+
+	if(iResult != 0)
+	{
+		printf("error at WSAStartup \n");
+		return -1;
+	}
+	#endif
+
+	int cSocket;
+	cSocket = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP);
+	if(cSocket < 0)
+	{
+		errorHandler("socket creation failed. \n");
+		closesocket(cSocket);
+		ClearWinSock();
+		return -1;
+	}
+
+	struct sockaddr_in sad;
+	memset(&sad, 0, sizeof(sad));
+	sad.sin_family = AF_INET;
+	sad.sin_addr.s_addr = inet_addr("127.0.0.1");
+	sad.sin_port = htons(5193);
+
+	if(connect(cSocket, (struct sockaddr *)&sad, sizeof(sad)) < 0)
+	{
+		errorHandler("Failed to connect. \n");
+		closesocket(cSocket);
+		ClearWinSock();
+		return -1;
+	}
+
+	/*
+	char *inputString = "prova";
+	int stringLen = strlen(inputString);
+
+	if(send(cSocket, inputString, stringLen, 0) != stringLen)
+	{
+		errorHandler("send() sent a different number of bytes than expected.\n");
+		closesocket(cSocket);
+		ClearWinSock();
+		return -1;
+	}
+	*/
+
+	int totByteRcv = 0;
+	char buf[BUFFERSIZE];
+	totByteRcv = recv(cSocket, buf, BUFFERSIZE -1, 0);
+	buf[totByteRcv] = '\0';
+	printf("%s", buf);
+
+	printf("\nPrima parola   ---> ");
+	scanf("%s",buf);
+	sendSuccess(cSocket,buf);
+	memset(buf,0,sizeof(buf));
+	printf("Seconda parola   ---> ");
+	scanf("%s",buf);
+	sendSuccess(cSocket,buf);
+	memset(buf,0,sizeof(buf));
+
+	totByteRcv = recv(cSocket, buf, BUFFERSIZE -1, 0);
+	buf[totByteRcv] = '\0';
+	printf("%s\n", buf);
+	memset(buf,0,sizeof(buf));
+	totByteRcv = recv(cSocket, buf, BUFFERSIZE -1, 0);
+	buf[totByteRcv] = '\0';
+	printf("%s\n", buf);
+	memset(buf,0,sizeof(buf));
+	/*
+	while(totByteRcv < stringLen)
+	{
+		if((byteReceived = recv(cSocket, buf, BUFFERSIZE -1, 0 )) <= 0)
+		{
+			errorHandler("recv() failed or connection closed prematurely\n");
+			closesocket(cSocket);
+			ClearWinSock();
+			return -1;
+		}
+		totByteRcv += byteReceived;
+		buf[byteReceived] = '\0';
+		printf("%s", buf);
+	}
+	*/
+
+	closesocket(cSocket);
+	ClearWinSock();
+	printf("\n");
+	system("pause");
+	return(0);
+
+}
---
+++ 1_esercitazioneRetiServer/src/Server.c
@@ -0,0 +1,169 @@
+/*
+ ============================================================================
+ Name        : 1_esercitazioneReti.c
+ Author      : 
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+#if defined WIN32
+#include <winsock.h>
+#else
+#define closesocket close
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#endif
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#define PROTOPORT 5193
+#define QLEN 6
+#define BUFFERSIZE 512
+
+void errorHandler(char *errorMessage)
+{
+	printf("%s", errorMessage);
+}
+
+void ClearWinSock()
+{
+	#if defined WIN32
+		WSACleanup();
+	#endif
+}
+
+int sendToClient(int clientSocket, const char *message)
+{
+	int lungMessage = strlen(message);
+	if(send(clientSocket, message, lungMessage, 0) != lungMessage)
+	{
+		errorHandler("send() sent a different number of bytes than expected.\n");
+		closesocket(clientSocket);
+		ClearWinSock();
+		return -1;
+	}
+	return 0;
+}
+
+void sendSuccess(int clientSocket, const char *message)
+{
+	int success;
+	success = sendToClient(clientSocket, message);
+}
+
+void ToUpper(char *message)
+{
+	int lung = strlen(message);
+	int i;
+	for(i = 0; i < lung; i++)
+	{
+		message[i] = toupper(message[i]);
+	}
+}
+
+void ToLower(char *message)
+{
+	int lung = strlen(message);
+	int i;
+	for(i = 0; i < lung; i++)
+	{
+		message[i] = tolower(message[i]);
+	}
+}
+
+int main(int argc, char *argv[])
+{
+	int port;
+	if (argc > 1)
+	{
+		port = atoi(argv[1]);
+	}
+	else
+	{
+		port = PROTOPORT;
+		if(port < 0)
+		{
+			printf("bad port number %s \n", argv[1]);
+			return 0;
+		}
+	}
+
+	#if defined WIN32
+	WSADATA wsaData;
+	int iResult = WSAStartup(MAKEWORD(2,2), &wsaData);
+
+	if (iResult != 0)
+	{
+		errorHandler("Error at WSAStartup()\n");
+		return 0;
+	}
+	#endif
+
+	int mySocket;
+	mySocket = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP);
+	if (mySocket < 0)
+	{
+		errorHandler("socket creation failed. \n");
+		ClearWinSock();
+		return -1;
+	}
+
+	struct sockaddr_in sad;
+	memset(&sad, 0, sizeof(sad));
+	sad.sin_family = AF_INET;
+	sad.sin_addr.s_addr = inet_addr("127.0.0.1");
+	sad.sin_port = htons(port);
+
+	if(bind(mySocket, (struct sockaddr*) &sad, sizeof(sad)) < 0)
+	{
+		errorHandler("bind() failed. \n");
+		closesocket(mySocket);
+		ClearWinSock();
+		return -1;
+	}
+
+	if(listen(mySocket, QLEN) < 0)
+	{
+		errorHandler("listen() failed. \n");
+		closesocket(mySocket);
+		ClearWinSock();
+		return -1;
+	}
+
+	struct sockaddr_in cad;
+	int clientSocket;
+	int clientLen;
+	printf("Waiting for a client to connect... \n");
+	while(1)
+	{
+		clientLen = sizeof(cad);
+		if((clientSocket = accept(mySocket, (struct sockaddr *)&cad, &clientLen)) < 0)
+		{
+			closesocket(mySocket);
+			ClearWinSock();
+			return 0;
+		}
+		printf("Hading a client %s\n", inet_ntoa(cad.sin_addr));
+
+		sendSuccess(clientSocket,"Connessione avvenuta");
+
+		char word1[BUFFERSIZE], word2[BUFFERSIZE];
+		int totByteRcv = recv(clientSocket, word1, BUFFERSIZE - 1, 0);
+		word1[totByteRcv] = '\0';
+		printf("%s\n",word1);
+		totByteRcv = recv(clientSocket, word2, BUFFERSIZE - 1, 0);
+		word2[totByteRcv] = '\0';
+		printf("%s\n",word2);
+
+		ToUpper(word1);
+		ToLower(word2);
+
+		sendSuccess(clientSocket, word1);
+		sendSuccess(clientSocket, word2);
+
+	}
+
+	return EXIT_SUCCESS;
+}
